7e39138a12d3e298ca227a977f54f230661443a1
Студент <student@rivne.office.com>
2025-10-02 10:58:50 +0300

    додавання інформації

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,34 +1,33 @@
 ﻿using System;
 
-abstract class Shape
+abstract class Shape //клас не можна створити напряму
 {
-    public abstract double GetArea();
-    public abstract void PrintInfo();
+    public abstract double GetArea(); //метод для обчислення площі
+    public abstract void PrintInfo(); //виведення інфо про фігуру
 }
-
 class Circle : Shape
 {
-    public double Radius { get; set; }
+    public double Radius { get; set; } //radius - властивість кола
 
-    public Circle(double radius)
+    public Circle(double radius) //дозволяє створювати об’єкт із заданим радіусом
     {
         Radius = radius;
     }
 
-    public override double GetArea()
+    public override double GetArea() //обчислює площу
     {
         return Math.PI * Radius * Radius;
     }
 
-    public override void PrintInfo()
+    public override void PrintInfo() //виводить інформацію про коло
     {
         Console.WriteLine($"Фігура: Коло | Радіус = {Radius} | Площа = {GetArea():F2}");
     }
 }
 class Rectangle : Shape
 {
-    public double Width { get; set; }
-    public double Height { get; set; }
+    public double Width { get; set; } //ширина
+    public double Height { get; set; } //висота
 
     public Rectangle(double width, double height)
     {
@@ -51,14 +50,19 @@ class Program
 {
     static void Main()
     {
-        Shape[] shapes = new Shape[]
+        Shape[] shapes = new Shape[] //масив
         {
             new Circle(5),
             new Circle(2.3),
             new Rectangle(4, 6),
             new Rectangle(2.5, 3.8)
         };
-        foreach (var shape in shapes)
+        foreach (var shape in shapes) // викликається метод PrintInfo для кожної фігури
+        /*
+         *Поліморфізм: хоча тип змінної shape – Shape, 
+         *виконується саме той метод, який відповідає 
+         *реальному типу об’єкта (Circle або Rectangle).
+        */
         {
             shape.PrintInfo();
         }
