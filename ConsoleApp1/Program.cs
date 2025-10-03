67e33fa5e72880a308af04e411071fc735058c95
Студент <student@rivne.office.com>
2025-10-03 11:10:58 +0300

    ще раз перероблено

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,48 @@
 ﻿using System;
 class Shape
 {
-    public virtual double GetArea() => 0;
-    public virtual void PrintInfo() => Console.WriteLine("Невідома фігура");
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine("Невідома фігура");
+    }
 }
 class Circle : Shape
 {
     public double Radius { get; set; }
-    public Circle(double r) => Radius = r;
-
-    public override double GetArea() => Math.PI * Radius * Radius;
-    public override void PrintInfo() =>
-        Console.WriteLine($"Коло: Радіус={Radius}, Площа={GetArea():F2}");
+    public Circle(double r)
+    {
+        Radius = r;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Коло: Радіус = " + Radius + ", Площа = " + GetArea().ToString("F2"));
+    }
 }
 class Rectangle : Shape
 {
     public double Width { get; set; }
     public double Height { get; set; }
-    public Rectangle(double w, double h) { Width = w; Height = h; }
-
-    public override double GetArea() => Width * Height;
-    public override void PrintInfo() =>
-        Console.WriteLine($"Прямокутник: Ширина={Width}, Висота={Height}, Площа={GetArea():F2}");
+    public Rectangle(double w, double h)
+    {
+        Width = w;
+        Height = h;
+    }
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+    public override void PrintInfo()
+    {
+        Console.WriteLine("Прямокутник: Ширина = " + Width + ", Висота = " + Height + ", Площа = " + GetArea().ToString("F2"));
+    }
 }
 class Program
 {
@@ -34,7 +55,9 @@ class Program
             new Rectangle(4, 6),
             new Rectangle(2.5, 3.8)
         };
-
-        foreach (var s in shapes) s.PrintInfo();
+        foreach (var s in shapes)
+        {
+            s.PrintInfo();
+        }
     }
 }
