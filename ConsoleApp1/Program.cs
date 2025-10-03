76c4126e0e98115a4c215ab79981a384a31646f4
Студент <student@rivne.office.com>
2025-10-03 10:49:16 +0300

    новий код

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,72 +1,40 @@
 ﻿using System;
-
-abstract class Shape //клас не можна створити напряму
+class Shape
 {
-    public abstract double GetArea(); //метод для обчислення площі
-    public abstract void PrintInfo(); //виведення інфо про фігуру
+    public virtual double GetArea() => 0;
+    public virtual void PrintInfo() => Console.WriteLine("Невідома фігура");
 }
 class Circle : Shape
 {
-    public double Radius { get; set; } //radius - властивість кола
+    public double Radius { get; set; }
+    public Circle(double r) => Radius = r;
 
-    public Circle(double radius) //дозволяє створювати об’єкт із заданим радіусом
-    {
-        Radius = radius;
-    }
-
-    public override double GetArea() //обчислює площу
-    {
-        return Math.PI * Radius * Radius;
-    }
-
-    public override void PrintInfo() //виводить інформацію про коло
-    {
-        Console.WriteLine($"Фігура: Коло | Радіус = {Radius} | Площа = {GetArea():F2}");
-    }
+    public override double GetArea() => Math.PI * Radius * Radius;
+    public override void PrintInfo() =>
+        Console.WriteLine($"Коло: Радіус={Radius}, Площа={GetArea():F2}");
 }
 class Rectangle : Shape
 {
-    public double Width { get; set; } //ширина
-    public double Height { get; set; } //висота
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public Rectangle(double w, double h) { Width = w; Height = h; }
 
-    public Rectangle(double width, double height)
-    {
-        Width = width;
-        Height = height;
-    }
-
-    public override double GetArea()
-    {
-        return Width * Height;
-    }
-
-    public override void PrintInfo()
-    {
-        Console.WriteLine($"Фігура: Прямокутник | Ширина = {Width}, Висота = {Height} | Площа = {GetArea():F2}");
-    }
+    public override double GetArea() => Width * Height;
+    public override void PrintInfo() =>
+        Console.WriteLine($"Прямокутник: Ширина={Width}, Висота={Height}, Площа={GetArea():F2}");
 }
-
 class Program
 {
     static void Main()
     {
-        Shape[] shapes = new Shape[] //масив
+        Shape[] shapes =
         {
             new Circle(5),
             new Circle(2.3),
             new Rectangle(4, 6),
             new Rectangle(2.5, 3.8)
         };
-        foreach (var shape in shapes) // викликається метод PrintInfo для кожної фігури
-        /*
-         *Поліморфізм: хоча тип змінної shape – Shape, 
-         *виконується саме той метод, який відповідає 
-         *реальному типу об’єкта (Circle або Rectangle).
-        */
-        {
-            shape.PrintInfo();
-        }
 
-        Console.ReadLine();
+        foreach (var s in shapes) s.PrintInfo();
     }
 }
